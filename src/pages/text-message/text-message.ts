438351db95056e99338698cc453a0057cc5f13d1
Nathan Moll <nmoll@byteperceptions.com>
2017-07-29 10:56:35 -0500

    Add translations for text-message page

--- src/pages/text-message/text-message.ts
+++ src/pages/text-message/text-message.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavParams, NavController, ToastController } from 'ionic-angular';
 import { SMS } from '@ionic-native/sms';
+import { TranslateService } from '@ngx-translate/core';
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'page-text-message',
@@ -9,23 +11,26 @@ import { SMS } from '@ionic-native/sms';
 export class TextMessagePage {
 
   phoneNumbers: Array<string>;
-  message: string = 'Try out the new LLConnect app!';
+  textMessage: string;
+  messages: any;
 
   constructor(
     private sms: SMS,
     navParams: NavParams,
     private navCtrl: NavController,
-    private toastCtrl: ToastController
+    private toastCtrl: ToastController,
+    private translateService: TranslateService
   ) {
     this.phoneNumbers = navParams.get('phoneNumbers');
+    this.loadMessages();
   }
 
   sendMessage(): void {
-    this.sms.send(this.phoneNumbers, this.message).then(() => {
+    this.sms.send(this.phoneNumbers, this.textMessage).then(() => {
       this.navCtrl.popToRoot();
-      this.showToast('Message Sent!');
-    }, () => {
-      this.showToast('Unable to send message.');
+      this.showToast(this.messages.MESSAGE_SEND_SUCCESS);
+    }, (err) => {
+      this.showToast(this.messages.MESSAGE_SEND_FAILURE + ' ' + JSON.stringify(err));
     });
   }
 
@@ -36,6 +41,17 @@ export class TextMessagePage {
       position: 'bottom'
     });
     toast.present();
+  }
+
+  loadMessages(): void {
+    this.translateService.get([
+      'INVITATION_MESSAGE_PLACEHOLDER',
+      'MESSAGE_SEND_SUCCESS',
+      'MESSAGE_SEND_FAILURE'
+    ]).subscribe(values => {
+      this.messages = values;
+      this.textMessage = values.INVITATION_MESSAGE_PLACEHOLDER;
+    });
   }
 
 }
